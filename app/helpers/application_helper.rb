9b5d6e10fbb52ef4e555ed3d802edf23e06226f2
William Krieger <williamt@williamt.com>
2016-12-03 17:53:42 +0000

    cleanup of login, home, reports, and other static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # Returns random artwork file name
+  def random_artwork
+    images = [ "assets/images/art/bee1.jpg", "assets/images/art/butterfly1.jpg", 
+          "assets/images/art/butterfly2.jpg", "assets/images/art/dragonfly1.jpg",
+          "assets/images/art/flower1.jpg", "assets/images/art/flower2.jpg",
+          "assets/images/art/flower3.jpg", "assets/images/art/flower4.jpg",
+          "assets/images/art/flower5.jpg", "assets/images/art/flower6.jpg",
+          "assets/images/art/flower7.jpg", "assets/images/art/flower8.jpg",
+          "assets/images/art/ladybug1.jpg" ]
+    images[rand(images.size)]
+  end
 end
